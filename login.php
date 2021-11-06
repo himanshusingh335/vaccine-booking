d10858b29b6ba19c29b4b97bba80058978245be0
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2021-11-06 13:25:44 +0530

    💄 add website name on login and signup

--- login.php
+++ login.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Login</title>
+    <title>Get CovidFreeNated- Login</title>
     <link rel="stylesheet" href="auth-style.css"/>
 </head>
 <body>
@@ -36,7 +36,7 @@
     } else {
 ?>
     <form class="form" method="post" name="login">
-        <h1 class="login-title">Login</h1>
+        <h1 class="login-title">Get CovidFreeNated- Login</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
         <input type="password" class="login-input" name="password" placeholder="Password"/>
         <input type="submit" value="Login" name="submit" class="login-button"/>
--- registration.php
+++ registration.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <title>Registration</title>
+    <title>Get CovidFreeNated- Registration</title>
     <link rel="stylesheet" href="auth-style.css"/>
 </head>
 <body>
@@ -33,7 +33,7 @@
     } else {
 ?>
     <form class="form" action="" method="post">
-        <h1 class="login-title">Registration</h1>
+        <h1 class="login-title">Get CovidFreeNated- Registration</h1>
         <input type="text" class="login-input" name="username" placeholder="Username" required />
         <input type="text" class="login-input" name="email" placeholder="Email Adress">
         <input type="password" class="login-input" name="password" placeholder="Password">

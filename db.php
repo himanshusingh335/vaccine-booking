ebaa2fa982f132a1ae40dbe84b9689eb2cd4dba4
Himanshu Singh <61236944+himanshusingh335@users.noreply.github.com>
2021-12-15 10:59:04 +0530

    🗃️ change db path

--- db.php
+++ db.php
@@ -1,5 +1,15 @@
 <?php
-    $con = mysqli_connect("localhost","root","","LoginSystem");
+   $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+   $cleardb_server = $cleardb_url["host"];
+   $cleardb_username = $cleardb_url["user"];
+   $cleardb_password = $cleardb_url["pass"];
+   $cleardb_db = substr($cleardb_url["path"],1);
+   $active_group = 'default';
+   $query_builder = TRUE;
+   // Connect to DB
+   $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
+   echo $cleardb_db;
     
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
